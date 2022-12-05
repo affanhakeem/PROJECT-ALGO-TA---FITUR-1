de955317051ea7b272c44724a55aefb276f8185a
affanhakeem <112384955+affanhakeem@users.noreply.github.com>
2022-12-05 16:24:11 +0700

    Update Coba-Coba.py

--- Coba-Coba.py
+++ Coba-Coba.py
@@ -2,6 +2,7 @@ import os
 os.system("cls")
 from tabulate import tabulate
 from datetime import datetime
+import pandas as pd
 
 def mulai():
     print("""
@@ -23,7 +24,6 @@ mulai()
     
 
 a = int(input("=> "))
-
 
 os.system("cls")
 def tahap2(ltr, nutri):
@@ -56,9 +56,8 @@ Dalam setiap kali penjadwalan\n(!!! 3 HARI SEKALI !!!)\n
             pass
             os.system("cls")
 
-if a == 1:
-    while True :
-        print("""
+def pilih_syr(mulai):
+    print("""
 ===================
 |   PILIH SAYUR   |
 ===================
@@ -69,137 +68,139 @@ if a == 1:
 
 [Z] KEMBALI
 """)
-        sayur = (input("=> "))
-        if sayur.lower() == "a" :
-            jdl = """
+    sayur = (input("=> "))
+    if sayur.lower() == "a" :
+        jdl = """
 ==========================
 |   Kamu telah memilih   |
 |      SAYUR SELADA      |                            
 ==========================
 """
-            baw = 0
-            bak = 28
-            ltr = 1.5
-            nutri = 5
+        baw = 0
+        bak = 28
+        ltr = 1.5
+        nutri = 5
 
-        elif sayur.lower() == "b":
-            jdl = """
+    elif sayur.lower() == "b":
+        jdl = """
 ==========================
 |   Kamu telah memilih   |
 |     SAYUR KANGKUNG     |                            
 ==========================
 """
-            baw = 33
-            bak = 48
-            ltr = 1.5
-            nutri = 5
+        baw = 33
+        bak = 48
+        ltr = 1.5
+        nutri = 5
 
-        elif sayur.lower() == "c" :
-            jdl = """
+    elif sayur.lower() == "c" :
+        jdl = """
 ==========================
 |   Kamu telah memilih   |
 |      SAYUR PAKCOY      |                            
 ==========================
 """
-            baw = 53
-            bak = 76
-            ltr = 1.5
-            nutri = 5
+        baw = 53
+        bak = 76
+        ltr = 1.5
+        nutri = 5
 
-        elif sayur.lower() == "d" :
-            jdl = """
+    elif sayur.lower() == "d" :
+        jdl = """
 ==========================
 |   Kamu telah memilih   |
 |     SAYUR SELEDRI      |                            
 ==========================
 """
-            baw = 80
-            bak = 109
-            ltr = 1.5
-            nutri = 5
-        elif sayur.lower() == "z" :
-            os.system("cls")
-            mulai()
-            continue
-        else :
-            os.system("cls")
-            print("\n!!! Tolong masukkan kode dengan benar !!!")
-            continue
+        baw = 80
+        bak = 109
+        ltr = 1.5
+        nutri = 5
+    elif sayur.lower() == "z" :
+        os.system("cls")
+        mulai()
+    else :
+        os.system("cls")
+        print("\n!!! Tolong masukkan kode dengan benar !!!")
+    os.system("cls")
+    return jdl,baw,bak,ltr,nutri
+
+def tutor_syr(jdl, baw, bak):
+    while True :
+        print(jdl)
+        print("\nSEBELUM MELAKUKAN PENANAMAN SAYUR SELADA PADA HYDROPONIC, YUK SIMAK DULU TUTORIAL DI BAWAH INI YA\n")
+        rd = open("tutorial.txt", "r")
+        var = rd.readlines()[baw:bak]
+        for o in var:
+            print(o, end="")
+
+        nt2 = input("\nApakah sudah paham?\n[Y/N]\n=> ")
+        if nt2.lower() == "y":
+            break
+        elif nt2.lower() == "n":
+            pass
         os.system("cls")
 
-        pass
+def penjadwalan():
+    os.system("cls")
+    tgl = datetime.now().strftime("%d/%m/%y")
+    inp1 = int(input("Penjadwalan pemberian nutrisi ke => "))
+    inp2 = float(input("PH air saat ini-> "))
+    print("\n")
+    if inp2 < 6:
+        lar = "PH UP"
+    elif inp2 > 7:
+        lar = "PH DOWN"
+    else:
+        lar = "NETRAL"
 
-        while True :
-            print(jdl)
-            print("\nSEBELUM MELAKUKAN PENANAMAN SAYUR SELADA PADA HYDROPONIC, YUK SIMAK DULU TUTORIAL DI BAWAH INI YA\n")
-            rd = open("tutorial.txt", "r")
-            var = rd.readlines()[baw:bak]
-            for o in var:
-                print(o, end="")
+    opn= open("data.txt","a")
+    opn.write(f"{tgl},{inp1},{inp2},{lar}\n")
+    opn.close()
 
-            nt2 = input("\nApakah sudah paham?\n[Y/N]\n=> ")
-            if nt2.lower() == "y":
-                break
-            elif nt2.lower() == "n":
-                pass
-            os.system("cls")
-
-
-        tahap2(ltr, nutri)
-
-        import pandas as pd
-        tgl = datetime.now().strftime("%d/%m/%y")
-        while True:
-            os.system("cls")
-            inp1 = int(input("Penjadwalan pemberian nutrisi ke => "))
-            inp2 = float(input("PH air saat ini-> "))
-            print("\n")
-            if inp2 < 6:
-                lar = "PH UP"
-            elif inp2 > 7:
-                lar = "PH DOWN"
-            else:
-                lar = "NETRAL"
-
-            opn= open("data.txt","a")
-            opn.write(f"{tgl},{inp1},{inp2},{lar}\n")
-            opn.close()
-
-            note = pd.read_csv("data.txt")
-            port = note.to_csv("data.csv", index = None)
-            list = pd.read_csv("data.csv")
-            print(tabulate(list.iloc[0:20], headers="keys", tablefmt="fancy_grid", showindex=False))
-            print("""
+    note = pd.read_csv("data.txt")
+    port = note.to_csv("data.csv", index = None)
+    list = pd.read_csv("data.csv")
+    print(tabulate(list.iloc[0:30], headers="keys", tablefmt="fancy_grid", showindex=False))
+    print("""
 !!! NB !!!
 [1] JIKA KETERANGAN LARUTAN "PH DOWN" PERLU DIBERIKAN LARUTAN PH DOWN
 [2] JIKA KETERANGAN LARUTAN "PH UP" PERLU DIBERIKAN LARUTAN PH UP
 [3] JIKA KETERANGAN LARUTAN "NETRAL" PH AIR SUDAH STABIL
 """)
 
-            while True :
-                inp3 = input("\nApakah sudah panen?\n[Y/N]\n=> ")
-                if inp3.lower() == "n" :
-                    pass    
-                elif inp3.lower() == "y" :
-                    print("Apakah kamu yakin dengan pilihanmu? Jika benar-benar sudah panen maka data penjadwalan akan terhapus")
+if a == 1:
+        jdl, baw, bak, ltr, nutri = pilih_syr(mulai)
+        tutor_syr(jdl, baw, bak)
+        tahap2(ltr, nutri)
+        penjadwalan()
 
-                    finish= input("\nApakah betul sudah panen? \n[Y/N]\n=> ")
-                    if finish.lower() == 'n':
-                        pass
-                    elif finish.lower() == "y":
-                        opn= open("data.txt","w")
-                        opn.write("Tanggal,Jadwal,PH,Larutan\n")
-                        opn.close()
-                        print("""
+        while True :
+            inp3 = input("\nApakah sudah panen?\n[Y/N]\n=> ")
+            if inp3.lower() == "n" :
+                penjadwalan()
+                continue    
+            elif inp3.lower() == "y" :
+                print("Apakah kamu yakin dengan pilihanmu? Jika benar-benar sudah panen maka data penjadwalan akan terhapus")
+
+                finish= input("\nApakah betul sudah panen? \n[Y/N]\n=> ")
+                if finish.lower() == 'n':
+                    penjadwalan()
+                    continue
+                elif finish.lower() == "y":
+                    opn= open("data.txt","w")
+                    opn.write("Tanggal,Jadwal,PH,Larutan\n")
+                    opn.close()
+                    print("""
 ============================
 |   SELAMAT TANAMAN KAMU   |
 |       TELAH PANEN!       |                                
 ============================
 """)
-                    break  
-                break
+                        
             break
-        
+
+
 
     # elif sayur.lower() == "b" :
     #     print("\nKamu telah memilih sayur Kangkung\n")
